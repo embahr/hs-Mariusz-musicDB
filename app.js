fb12f172ce57d29b7d92a2e1e77f2d461d61cc42
Mariusz Bachurski <info@mbachurski.com>
2017-02-10 21:27:03 -0800

    Updated endpoints for /follow and /listen, Migrated DB
    - Migrated DB from local to mlab
    - /follow endpoint now POSTs new relationship (example use:  “{“from”:
    “a”, “to”: “e”}
    - /listen/:user endpoint now returns array that a particular user has
    listened to
    - /listen endpoint POSTs music listened to by user (example use:
    {“user”: “e”, “music”: “m10”}

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 Users = require('./models/users');
 
 // Connect node.js to mongodb
-mongoose.connect('mongodb://localhost/musicdb');
+mongoose.connect('mongodb://develop:project@ds145329.mlab.com:45329/musicdb');
 
 // Mongoose debugger
 mongoose.set('debug', true);
@@ -24,17 +24,73 @@ app.get('/users', function(req, res) {
       console.log("Unable to retrieve current list of users...");
     }
     res.json(users);
+    console.log(users);
   });
 });
 
 // POST new follow relationship to user
 app.post('/follow', function(req, res) {
+  var user = req.body.from,
+      toFollow = req.body.to,
+      selfIndex = toFollow.indexOf(user), //Check to see if user is trying to follow themself
+      following = [],
+      followIndex = following.indexOf(toFollow);
 
+   // TO DO: check if user is already following user to be added
+   // can't update 'var following' from the callback...
+/*
+    Users.getFollows(user, function(err, follow) {
+      if (err) {
+        throw err;
+      } else {
+          res.json(follow[0].follows);
+          console.log(following);
+
+      }
+    });
+    //console.log(following);
+*/
+
+// Check if user is trying to follow themself
+  if (selfIndex == -1) {
+      // Check if user already follows user to be added
+    if (followIndex !== -1) {
+      //console.log("You are already following " + toFollow + ".");
+    } else {
+      Users.addFollow(user, {"follows": toFollow}, {new: true, upsert: true}, function(err, result) {
+      res.send("You are now following " + toFollow + ".");
+      console.log("You are now following " + toFollow + ".");
+      });
+    }
+  } else {
+   res.send(user + ", you can't follow yourself!");
+   console.log(user +", you can't follow yourself!");
+  }
+});
+
+// GET music listened to by user
+app.get('/listen/:user', function(req, res) {
+  var user = req.params.user;
+  Users.getUserListens(user, function(err, music) {
+    if (err) {
+      throw err;
+    }
+    res.json(music[0].music);
+  });
 });
 
 // POST music listened to by user
 app.post('/listen', function(req, res) {
-
+  var user = req.body.user,
+      music = req.body.music;
+  Users.addListen(user, {music: music}, {new: true}, function(err, list) {
+    if (err) {
+      throw err;
+    }
+    res.json(list.music);
+    console.log(user + " has just listened to " + music);
+    console.log(list.music);
+  });
 });
 
 // GET music recommendations
--- models/users.js
+++ models/users.js
@@ -28,3 +28,33 @@ var Users = mongoose.model('Users', usersSchema);
 module.exports.getUsers = function(callback) {
   Users.find(callback);
 };
+
+// GET user's follows
+module.exports.getFollows = function(user, callback) {
+  var query = { user: user };
+      Users.find(query, {follows: 1, _id: 0}, callback);
+};
+
+// POST new follow relationship to user
+module.exports.addFollow = function(user, follow, options, callback) {
+  var from = { user: user },
+      to = {
+        $addToSet: { follows: follow.follows  }
+      };
+  Users.findOneAndUpdate(from, to, options, callback);
+};
+
+// GET music listened to by user
+module.exports.getUserListens = function(user, callback) {
+  var query = { user: user };
+      Users.find(query, {music: 1, _id: 0}, callback);
+};
+
+// POST music listened to by user
+module.exports.addListen = function(user, hear, options, callback) {
+  var query = { user: user },
+      lis = {
+        $addToSet: { music: hear.music }
+      };
+    Users.findOneAndUpdate(query, lis, options, callback);
+};
