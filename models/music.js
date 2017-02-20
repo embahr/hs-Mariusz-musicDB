16b1dfba30b2d8a22d18f5facdafdb8540ab7cbb
Mariusz Bachurski <info@mbachurski.com>
2017-02-19 21:24:49 -0800

    Reworked getSongs function
    - getSongs is now more accurate, and shows results for most relevant
    results first.

--- models/music.js
+++ models/music.js
@@ -21,12 +21,18 @@ module.exports.getMusic = function(callback) {
 
 // GET tags related to listens (docs.music) from user
 module.exports.getTags = function(songs, callback) {
-  Music.find({ name: {$in: songs}}, {name: 1, tags: 1, _id: 0}, callback);
-  console.log('***Songs the user has listened to: ' + songs);
+  Music.find({ name: {$in: songs}}, {tags: 1, _id: 0}, callback);
+//  console.log('***Songs the user has listened to: ' + songs);
 };
 
 // GET songs that share tags with results of getTags
-module.exports.getSongs = function(tags, callback) {
-  Music.find({ tags: {$in: tags}}, {name: 1, tags: 1, _id: 0}, callback);
-  console.log('***Tags of the songs the user has listened to: ' + tags);
+module.exports.getSongs = function(tags, tags2, callback) {
+  tags.every(function(tagCheck, ch) {
+    Music.find({ $or : [
+      { $and : [ { tags : {$in: [tags[ch]]} }, { tags : {$in: [tags2[ch]] }} ] },
+      { $or : [ { tags : { $in: [tags[ch]]} }, { tags : { $in: [tags2[ch]]}} ] }
+    ]} , {name: 1, tags: 1, _id: 0}, callback).sort({ tags: -1});
+    console.log(tags);
+  });
+//console.log('***Tags of the songs the user has listened to: ' + tags);
 };
